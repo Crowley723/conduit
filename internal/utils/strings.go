f187345ff585addf12172d0ed9d5233b7d925a8e
Brynn Crowley <littlehill723@gmail.com>
2026-04-14 10:05:28 -0700

    refactor: relicense
    Signed-off-by: Brynn Crowley <littlehill723@gmail.com>

--- internal/utils/strings.go
+++ internal/utils/strings.go
@@ -1,7 +1,6 @@
 // Copyright 2025 James D Elliot
 // Licensed under the Apache License, Version 2.0
 // Originally from: https://github.com/authelia/authelia
-// See APACHE-LICENSE.txt for full license text
 
 package utils
 
